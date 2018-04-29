53c8ae9c9c85263d9950e6f83cfd4b0259acfa0b
Z <zzettochan@gmail.com>
2018-04-29 23:18:47 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//test
 Route::get('/trangchu', function () {
     return view('welcome');
 })->name('homepage')->middleware('kiemtraDangnhap');
